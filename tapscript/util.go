987274eac84a9ec801585a1a658aae72f21ec12d
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2024-01-12 18:05:42 -0500

    tapscript: util to compute taptree sibling hash
    In this commit, we add logic to compute the root hash for various types
    of tapscript tree. This hash is used alongside the TapCommitment root
    to compute a final Taproot key.

--- tapscript/util.go
+++ tapscript/util.go
@@ -1,14 +1,18 @@
 package tapscript
 
 import (
+	"fmt"
+
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcwallet/waddrmgr"
 	"github.com/btcsuite/btcwallet/wallet/txsizes"
 	"github.com/lightninglabs/taproot-assets/commitment"
+	"github.com/lightninglabs/taproot-assets/fn"
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 )
 
@@ -34,6 +38,55 @@ func PayToTaprootScript(taprootKey *btcec.PublicKey) ([]byte, error) {
 		AddOp(txscript.OP_1).
 		AddData(schnorr.SerializePubKey(taprootKey)).
 		Script()
+}
+
+// TapTreeToSibling constucts a taproot sibling hash from a Tapscript
+// tree, to be used with a TapCommitment tree root to derive a tapscript root.
+func TapTreeToSibling(tapTree waddrmgr.Tapscript) (*chainhash.Hash,
+	error) {
+
+	var tapLeafHash *chainhash.Hash
+	switch tapTree.Type {
+	case waddrmgr.TapscriptTypeFullTree:
+		// TODO(jhb): Is this the right way to handle a tree?
+		switch len(tapTree.Leaves) {
+		case 1:
+			// A single tapscript leaf must be verified to not be
+			// another Taproot Asset commitment before use.
+			tapPreimage := commitment.NewPreimageFromLeaf(
+				tapTree.Leaves[0],
+			)
+			err := tapPreimage.VerifyNoCommitment()
+			if err != nil {
+				return nil, err
+			}
+
+			tapLeafHash, err = tapPreimage.TapHash()
+			if err != nil {
+				return nil, err
+			}
+
+		default:
+			tree := txscript.AssembleTaprootScriptTree(
+				tapTree.Leaves...,
+			)
+			tapLeafHash = fn.Ptr(tree.RootNode.TapHash())
+		}
+
+	case waddrmgr.TapscriptTypePartialReveal:
+		tapLeafHash = (*chainhash.Hash)(tapTree.ControlBlock.RootHash(
+			tapTree.RevealedScript,
+		))
+
+	case waddrmgr.TaprootKeySpendRootHash:
+		tapLeafHash = (*chainhash.Hash)(tapTree.RootHash)
+
+	default:
+		return nil, fmt.Errorf("invalid tapscript tree type: %v",
+			tapTree.Type)
+	}
+
+	return tapLeafHash, nil
 }
 
 // EstimateFee provides a worst-case fee and vsize estimate for a transaction
